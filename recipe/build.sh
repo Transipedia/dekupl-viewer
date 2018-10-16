b071b15ad820662668dbbc1ad21359ed0796af92
Dimitri Larue <contact@dimitrilarue.fr>
2018-10-16 15:00:52 +0200

    fix: add toys

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p "$PREFIX"/usr/local/dekupl-viewer
 cp -r src/* "$PREFIX"/usr/local/dekupl-viewer
+cp -r toy "$PREFIX"/usr/local
 
 echo "### Install bin ###"
 ln -svf "$PREFIX"/usr/local/dekupl-viewer/dekupl-viewer.sh "$PREFIX"/bin/dekupl-viewer
